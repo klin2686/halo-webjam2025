b42c187a296becf77ea3b5a9959f1cf23d7cba53
dolphinalt <ethan.zhao.us@gmail.com>
2025-11-06 17:35:45 -0800

    slight aesthetic changes to severity tag

--- frontend/Halo/src/components/SeverityTag.tsx
+++ frontend/Halo/src/components/SeverityTag.tsx
@@ -13,9 +13,9 @@ const SeverityTag = ({ severity, text }: SeverityTagProps) => {
       case "mild":
         return "bg-[rgba(183,255,83,0.44)]";
       case "none":
-        return "bg-[rgba(49, 49, 49, 0.44)]";
+        return "bg-gray-200";
       default:
-        return "bg-[rgba(49, 49, 49, 0.44)]";
+        return "bg-gray-200";
     }
   };
 
@@ -28,7 +28,7 @@ const SeverityTag = ({ severity, text }: SeverityTagProps) => {
       case "mild":
         return "bg-green-400";
       case "none":
-        return "bg-gray-300";
+        return "bg-white";
       default:
         return "bg-gray-300";
     }
@@ -36,12 +36,18 @@ const SeverityTag = ({ severity, text }: SeverityTagProps) => {
 
   return (
     <div
-      className={`h-[1.125rem] w-[5rem] rounded-full ${getSeverityColor(
+      className={`h-[1.125rem] w-[5.25rem] rounded-full ${getSeverityColor(
         severity
-      )} ${severity === "none" ? "border border-gray-400" : ""} flex items-center justify-center gap-[0.25rem]`}
+      )} ${
+        severity === "none" ? "" : ""
+      } flex items-center p-2 gap-[0.25rem]`}
     >
       <div className={`w-2 h-2 rounded-full ${getDotColor(severity)}`} />
-      <span className="text-sm text-black font-light font-sf-pro">{text}</span>
+      <div className="flex-1 flex items-center justify-center">
+        <span className="text-sm text-black font-light font-sf-pro">
+          {text}
+        </span>
+      </div>
     </div>
   );
 };
