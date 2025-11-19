e10b5e688b7b1dcf060786ab13150064a89c6cd5
IsaacPhoon <izackphoon@gmail.com>
2025-11-18 20:15:09 -0800

    Create AboutPage that combines both about and grid

---
+++ frontend/src/components/HaloAboutPage.tsx
@@ -0,0 +1,13 @@
+import HaloAbout from "./About";
+import HaloAboutGrid from "./HaloAboutGrid";
+
+const HaloAboutPage = () => {
+  return (
+    <>
+      <HaloAbout />
+      <HaloAboutGrid />
+    </>
+  );
+};
+
+export default HaloAboutPage;
